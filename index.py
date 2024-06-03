be463c1947d97070879b25a7c9f1e83623e6b142
rafinha285 <jcschrott10@gmail.com>
2024-06-03 05:50:23 -0300

    clan_info command

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import json;
 import nextcord
 from nextcord.ext import commands
 from nextcord import Interaction
+import asyncio
 
 
 import mongodb_utils
@@ -47,5 +48,55 @@ async def check_status(ctx: Interaction):
     else:
         await ctx.send("O servidor Minecraft está desligado.")
 
+@bot.slash_command(name="clan_info", description="Obtém informações sobre um clã")
+async def get_clan_command(interaction: nextcord.Interaction, clan_name: str):
+    try:
+        clan = await mongodb_utils.get_clan(clan_name)
+
+        clan_name = clan['name']
+        member_ids = clan['members']
+        members = []
+        points = clan['points']
+        owner = (await mongodb_utils.get_player(clan['owner']))["name"]
+        acronym = clan['prefix']
+        enemies_ids = clan["enemies"]
+        enemies = []
+        allies_ids = clan["allies"]
+        allies = []
+        
+        members_tasks = []
+        enemies_tasks = []
+        allies_tasks = []
+
+        for member_id in member_ids:
+            members_tasks.append(mongodb_utils.get_player(member_id))
+
+        for enemy_id in enemies_ids:
+            enemies_tasks.append(mongodb_utils.get_clan(enemy_id))
+
+        for ally_id in allies_ids:
+            allies_tasks.append(mongodb_utils.get_clan(ally_id))
+
+        members = await asyncio.gather(*members_tasks)
+        enemies = await asyncio.gather(*enemies_tasks)
+        allies = await asyncio.gather(*allies_tasks)
+
+        members_str = ", ".join(member["name"] for member in members)
+        enemies_str = ", ".join(enemy["name"] for enemy in enemies)
+        allies_str = ", ".join(ally["name"] for ally in allies)
+
+        response = (f"CLAN: {clan_name}\n"
+                    f"\tMembros: {members_str}\n"
+                    f"\tHonras: {points}\n"
+                    f"\tDono: {owner}\n"
+                    f"\tSigla: {acronym}\n"
+                    f"\tInimigos: {enemies_str}\n"
+                    f"\tAliados: {allies_str}")
+    except ValueError as e:
+        response = str(e)
+    if interaction.response.is_done():
+        await interaction.response.edit_message(content=response)
+    else:
+        await interaction.response.send_message(response)
 
 bot.run(TOKEN)
--- mongodb_utils.py
+++ mongodb_utils.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import json
+import mongo_types
 
 with open("./config.json") as config_file:
     configs = json.load(config_file)
@@ -16,9 +17,42 @@ db = client[MONGO_DATABASE]
 clan_collection = db[CLANS_COLLECTION]
 player_collection = db[PLAYER_COLLECTION]
 
-#player handles
-async def get_all_players():
+
+#~player handles
+async def get_all_players()->mongo_types.List[mongo_types.Player]:
     return await player_collection.find({})
 
-async def get_player():
-    return await player_collection.find
+#explicação dos tipos no arquivos de tipos
+async def get_player(player_id: str) -> mongo_types.Player:
+    player_data = player_collection.find_one({"_id": player_id})
+    if player_data:
+        return player_data
+    else:
+        raise ValueError(f"Player with ID {player_id} not found")
+
+# ta no nome oq essa função faz
+async def get_player_points(id:str)->int:
+    #retornara sempre um int por conta da database
+    return await player_collection.find({"_id":id}).points
+
+async def get_player_clan(id:str)->mongo_types.Clan:
+    clanName = player_collection.find({"_id":id}).clan
+    clanData = get_clan(clanName)
+    if clanData:
+        return clanData
+    else:
+        raise ValueError(f"Clan {clanName} not found")
+    
+
+
+#~clan handles
+async def get_clan(clanName:str)->mongo_types.Clan:
+    clan_data = clan_collection.find_one({"name": clanName})
+    print(clan_data)
+    if clan_data:
+        return clan_data
+    else:
+        raise ValueError(f"Clan {clanName} not found")
+    
+async def get_all_clans()->mongo_types.List[mongo_types.Clan]:
+    return await clan_collection.find({})
